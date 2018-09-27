71ca9c98411d616d49ec1c0c301061abf5bbb937
DIOS <joantiscar@gmail.com>
2018-09-27 19:59:37 +0200

    Primera versió

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/prova','ProvaController@show');
+Route::get('/prova',function(){
+    $prova ='aidfnaofnaofafa';
+    dd($prova);
+});
+Route::view('about','about');
+Route::view('contact','contact');
+Route::view('about','about');
+Route::view('about','about');
