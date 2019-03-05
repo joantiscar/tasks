8bb7bb2e869d073c482df0bd96bd006e55e9071a
DIOS <joantiscar@gmail.com>
2019-03-05 19:56:41 +0100

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.60174f36bb82377dab031ecdf6deaa7e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.e614a526621b6c603ddd89eafcdb2d37.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable no-undef */
 workbox.setConfig({
