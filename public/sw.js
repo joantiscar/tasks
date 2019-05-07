2b5fae5409d074890c5831bbddbbe7157962df4f
joantiscar <joantiscar@gmail.com>
2019-05-07 20:00:55 +0200

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,35 @@
-importScripts("/service-worker/precache-manifest.836a178831aaab25d2cd6547e83eb8bc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.9191ae6cd8f54ab0bb74e55f436c563f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
-/* eslint-disable no-undef */
-workbox.setConfig({
-  debug: true
-});
-workbox.skipWaiting()
-workbox.clientsClaim()
+workbox.setConfig({ debug: true })
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
 
-// workbox.routing.registerRoute(
-//   new RegExp('https://hacker-news.firebaseio.com'),
-//   workbox.strategies.staleWhileRevalidate()
-// );
+workbox.precaching.cleanupOutdatedCaches()
 
-// TODO cal utilitzar PushManager al registrar el service worker
 self.addEventListener('push', (event) => {
-  const title = 'TODO CANVIAR TITOL'
+  const title = 'Tasks App - Joan Tíscar'
   const options = {
     body: event.data.text()
+  }
+  event.waitUntil(self.registration.showNotification(title, options))
+})
+
+self.addEventListener('sync', function (event) {
+
+})
+
+const showNotification = () => {
+  self.registration.showNotification('Post Sent', {
+    body: 'You are back online and your post was successfully sent!'
+    // icon: 'assets/icon/256.png',
+    // badge: 'assets/icon/32png.png'
+  })
+}
+
+const bgSyncPlugin = new workbox.backgroundSync.Plugin('newsletter', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours
+  callbacks: {
+    queueDidReplay: showNotification
   }
 })
 
