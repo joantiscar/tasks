47c54e6c93a62916a4df59ccfb0492130212baff
joantiscar <joantiscar@gmail.com>
2019-05-28 19:27:44 +0200

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.b9cedb6fcf24dec8621f7eeb04c1ab89.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.03ec018ed9492d1196c5e66ad5131f71.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.setConfig({ debug: true })
 workbox.core.skipWaiting()
